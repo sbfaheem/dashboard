4255aaf1ff80582b20584cd4dd62d14a3b0ea1e4
sbfaheem <bilalfaheem47@gmail.com>
2026-04-04 00:42:33 +0500

    Fix tab navigation caching entirely

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import { fetchMonthlyReport } from "../../actions";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default async function Home({ searchParams }) {
   // Await search params specifically for Next 15 compatibility
   const params = await searchParams;
